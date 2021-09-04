49ac146bdc8bad4f9f5bf90c8033eea83f6bb117
Amarjeet Prajapati <amar141989@gmail.com>
2021-09-05 00:17:08 +0530

    Added class for reading the weather humidity and temparetaure and added class for Aws IOTinteraction

---
+++ AwsCloudHelper.py
@@ -0,0 +1,54 @@
+import boto3
+
+class AwsCloudHelper:
+
+    def __init__(self, region_name):
+        if region_name is None or len(region_name)==0:
+            self.iot_client = boto3.client('iot',region_name ='us-east-1')
+        else:
+            self.iot_client = boto3.client('iot',region_name)
+
+    def add_thing_to_thing_group(self):
+        pass
+
+    def attach_policy(self):
+        pass
+
+    def create_policy(self):
+        pass
+
+    def create_thing(self):
+        pass
+
+    def create_thing_group(self, group_name, group_description, parent_group_name):
+        response = self.iot_client.create_thing_group(
+        thingGroupName=group_name,
+        parentGroupName=parent_group_name,
+        thingGroupProperties={
+            'thingGroupDescription':group_description,
+            # 'attributePayload': {
+            #     'attributes': {
+            #         'string': 'string'
+            #     },
+            #     'merge': True|False
+            # }
+        },
+        tags=[
+            {
+                'Key': 'Test',
+                'Value': 'GroupTest'
+            },
+            ]
+        )
+        return response
+
+    def create_thing_type(self, type_name, type_desc):
+        response = self.iot_client.create_thing_type(
+        thingTypeName=type_name,
+        thingTypeProperties={
+            'thingTypeDescription': type_desc})
+        return response
+
+    def create_topic_rule(self):
+        pass
+
